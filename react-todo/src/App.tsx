32ebb9413b7442bb520b5aa333f82058957aba7c
UGangg <mororo_morori@naver.com>
2026-04-01 14:15:09 +0900

    [feat] contextAPI & components 분리

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css';
-import Todo from './components/Todo';
+import "./App.css";
+import Todo from "./components/Todo";
+import { TodoProvider } from "./context/TodoContext";
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <Todo />
-    </>
-  ); 
+    </TodoProvider>
+  );
 }
 
 export default App;
--- react-todo/src/components/Todo.tsx
+++ react-todo/src/components/Todo.tsx
@@ -1,92 +1,30 @@
-import { useState } from "react";
-import type { FormEvent } from "react";
-import type { TTodo } from "../types/todo";
+import { useTodo } from "../context/TodoContext";
+
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 const Todo = () => {
-  const [todos, setTodos] = useState<TTodo[]>([]);
-  const [doneTodos, setDoneTodos] = useState<TTodo[]>([]);
-  const [input, setInput] = useState<string>("");
-
-  console.log("Input", input);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log('동작');
-    const text = input.trim();
-
-    if (text) {
-      const newTodo: TTodo = { id: Date.now(), text };
-      setTodos((prevTodos) => [...prevTodos, newTodo]);
-      setInput('');
-    }
-  };
-
-  const completeTodo = (todo: TTodo) => {
-    setTodos((prevTodos) => prevTodos.filter((t) => t.id !== todo.id));
-    setDoneTodos((prevDonTodos) => [...prevDonTodos, todo]);
-  };
-
-  const deleteTodo = (todo: TTodo) => {
-    setDoneTodos((prevDoneTodo) => prevDoneTodo.filter((t) => t.id !== todo.id));
-
-  };
+	const {todos, completeTodo, deleteTodo, doneTodos} = useTodo();
 
   return (
     <div className="todo-container">
       <h1 className="todo-container__header">YONG TODO</h1>
-      <form onSubmit={handleSubmit} className="todo-container__form">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
-          id="todo-input"
-          className="todo-container__input"
-          placeholder="할 일 입력"
-          required
-        />
-        <button type="submit" className="todo-container__button">
-          할 일 추가
-        </button>
-      </form>
+      <TodoForm />
       <div className="render-container">
-        <div className="render-container__section">
-          <h2 className="render-container__title">할 일</h2>
-          <ul id="todo-list" className="render-container__list">
-            {todos.map((todo) => (
-              <li key={todo.id} className="render-container__item">
-                <span className="render-container__item-text">{todo.text}</span>
-                <button
-                  onClick={() => completeTodo(todo)}
-                  style={{
-                    backgroundColor: "green",
-                  }}
-                  className="render-container__item-button"
-                >
-                  완료
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="render-container__section">
-          <h2 className="render-container__title">완료</h2>
-          <ul id="done-list" className="render-container__list">
-            {doneTodos.map((todo) => (
-              <li key={todo.id} className="render-container__item">
-                <span className="render-container__item-text">{todo.text}</span>
-                <button
-                  onClick={() => deleteTodo(todo)}
-                  style={{
-                    backgroundColor: "red",
-                  }}
-                  className="render-container__item-button"
-                >
-                  삭제
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <TodoList
+          title="할 일"
+          todos={todos}
+          buttonLable="완료"
+          buttonColor="green"
+          onClick={completeTodo}
+        />
+        <TodoList
+          title="완료"
+          todos={doneTodos}
+          buttonLable="삭제"
+          buttonColor="red"
+          onClick={deleteTodo}
+        />
       </div>
     </div>
   );
