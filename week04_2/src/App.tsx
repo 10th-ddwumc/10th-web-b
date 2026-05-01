cdfc7acab74db21b309a6fe27c49e86dcb8efe5b
UGangg <mororo_morori@naver.com>
2026-05-02 05:38:37 +0900

    [feat]회원가입 기능 구현

--- week04_2/src/App.tsx
+++ week04_2/src/App.tsx
@@ -3,22 +3,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import "./App.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout />, // 🔥 layout 적용
+    element: <RootLayout />,
     errorElement: <NotFoundPage />,
     children: [
-      {
-        index: true, // "/" 경로
-        element: <HomePage />,
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
     ],
   },
 ]);
